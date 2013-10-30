be4d82003bcd9a8b6ba44050ba78326f7f17663c
Uroš Golja <goljau@comcom.si>
2013-10-30 15:25:32 +0100

    * added even more boilerplate code * fixed the configuration file to reflect real-world usage

--- logstash-symlink-index.sh
+++ logstash-symlink-index.sh
@@ -7,6 +7,7 @@ CONF_DRY_RUN=""
 CONF_FILE=""
 CONF_HELP=""
 CONF_INDEX_NAMES=""
+CONF_PRINT=""
 CONF_SOURCE_DIR=""
 
 log() {
@@ -42,13 +43,14 @@ run() {
   }
 
   local command="$*"
-  if [ -n "$CONF_DRY_RUN" ] ; then
+  [ -n "$CONF_DRY_RUN" ] && {
     echo "Would run '$command', however the CONF_DRY_RUN flag is set. Doing nothing."
     return 0
-  else
-    eval $command
-    return $?
-  fi
+  }
+  eval $command
+  local retval=$?
+  [ "$retval" -gt 0 ] && log_error "Failed: $run"
+  return $retval
 }
 
 check_CONF_DEBUG() {
@@ -113,30 +115,11 @@ check_CONF_SOURCE_DIR() {
   return $errors
 }
 
-#check_CONF_SOURCE_PATH() {
-  #local errors=0
-  #if [ ! -e "$CONF_SOURCE_PATH" ]; then
-    #log_error "Source index path '$CONF_SOURCE_PATH' does not exist."
-    #errors=$(( $errors + 1 ))
-  #elif [ ! -r "$CONF_SOURCE_PATH" ]; then
-    #log_error "Source index path '$CONF_SOURCE_PATH' is not readable."
-    #errors=$(( $errors + 1 ))
-  #elif [ ! -d "$CONF_SOURCE_PATH" ]; then
-    #log_error "Source index path '$CONF_SOURCE_PATH' is not a directory. This is unusual."
-    #errors=$(( $errors + 1 ))
-  #elif [ -L "$CONF_SOURCE_PATH" ]; then
-    #log_error "Source index path '$CONF_SOURCE_PATH' is a symlink. This index has probably already been moved."
-    #errors=$(( $errors + 1 ))
-  #fi
-  #echo $errors
-  #return $errors
-#}
-
 # Parse the command line arguments and build a running configuration from them.
 parse_args() {
   local args; args="$@"
-  local short_args="c:,d,f:,n,t:"
-  local long_args="config:,debug,dry-run,from-dir:,help,to-dir:"
+  local short_args="c:,d,f:,h,n,t:"
+  local long_args="config:,debug,dry-run,from-dir:,help,print-config,to-dir:"
   local g; g=$(getopt -o "$short_args" -l "$long_args" -- $args) || die "Could not parse arguments, aborting."
   log_debug "args: $args, getopt: $g"
 
@@ -172,6 +155,10 @@ parse_args() {
     elif [ "$a" = "-h" -o "$a" = "--help" ] ; then
       CONF_HELP="true"
 
+    # Help.
+    elif [ "$a" = "--print-config" ] ; then
+      CONF_PRINT="true"
+
     # Destination directory.
     elif [ "$a" = "-t" -o "$a" = "--to-dir" ] ; then
       shift
@@ -186,6 +173,40 @@ parse_args() {
   done
 
   return 0
+}
+
+print_help() {
+  cat <<HERE
+This shell script rsyncs a Logstash/elasticsearch index from source to 
+destination, removes the source and replaces it with a symlink to destination.
+See https://github.com/shkitch/logstash-symlink-index for details.
+
+Usage: logstash-symlink-index [options] <index_name> ... 
+
+Options are:
+  -f, --from-dir : source directory where indices are stored. Symlinks are created
+                   here, too.
+  -t, --to-dir   : Destination directory.
+
+  -c, --config       : Path to config file.
+      --print-config : Print the current configuration, then exit.
+  -d, --debug        : Enable debug output.
+  -n, --dry-run      : Don't do anyhing, just report what would be done.
+  -h, --help         : This text
+HERE
+}
+
+# Print the current configuration. This could be done more clevery in bash 
+# instead of dash but this would make the script shell-specific.
+print_config() {
+  log "CONF_DEBUG='$CONF_DEBUG'"
+  log "CONF_DEST_DIR='$CONF_DEST_DIR'"
+  log "CONF_DRY_RUN='$CONF_DRY_RUN'"
+  log "CONF_FILE='$CONF_FILE'"
+  log "CONF_HELP='$CONF_HELP'"
+  log "CONF_INDEX_NAMES='$CONF_INDEX_NAMES'"
+  log "CONF_PRINT='$CONF_PRINT'"
+  log "CONF_SOURCE_DIR='$CONF_SOURCE_DIR'"
 }
 
 # Build a running configuration from the configuration file.
@@ -203,6 +224,10 @@ load_conffile() {
     log_error "Configuration file '$fn' is not readable."
     errors=$(( $errors + 1 ))
   else 
+    # Fix the dash(1) stupidity when sourcing files in the current directory
+    [ "$(dirname $fn)" = "." ] && fn="./$fn"
+
+    # Do the stuff
     . $fn || { 
       log_error "Could not load configfile '$fn'."
       errors=$(( $errors + 1 ))
@@ -211,14 +236,73 @@ load_conffile() {
   return $errors
 }
 
-#
-# Do the whole command line arguments / configuration file lambada.
+symlink_index() {
+  local index_name="$1"
+  [ -z "$index_name" ] && {
+    log_warning "No index name given."
+    return 0
+  }
+  
+  local index_path; index_path="$CONF_SOURCE_DIR/$index_name"
+  
+  if [ ! -e "$index_path" ]; then
+    log_error "Source index path '$index_path' does not exist."
+    return 1
+  elif [ ! -r "$index_path" ]; then
+    log_error "Source index path '$index_path' is not readable."
+    return 1
+  elif [ ! -d "$index_path" ]; then
+    log_error "Source index path '$index_path' is not a directory. This is unusual."
+    return 1
+  elif [ -L "$index_path" ]; then
+    log_error "Source index path '$index_path' is a symlink. This index has probably already been moved."
+    return 1
+  fi
+
+  # rsync source index to the destination
+  run "rsync -a $index_path $CONF_DEST_DIR" || {
+    log_error ": $run"
+    return 1
+  }
+
+  # Remove the source index
+  run "rm -r $index_path" || {
+    log_error "Oops, something went wrong."
+    return 1
+  }
+
+  # Create the symlink in place of the source index
+  local symlink_path="$CONF_DEST_DIR/$index_name"
+  run "ln -s $symlink_path $CONF_SOURCE_DIR" || {
+    log_error "Oops, something went wrong."
+  }
+  
+  return 0
+}
+
+# Do the whole command line arguments / configuration file / help lambada in the
+# proper order.
 # 
-# First, parse the command line arguments to see if the user has given us a
-# configuration file. If so, load it. Then, parse the arguments again because
-# by convention they should override the configuration file.
+# First, parse the command line arguments. to see if the user has given us a
+# configuration file.
 parse_args $@
+#
+# Do the help thing if the user so wishes.
+[ -n "$CONF_HELP" ] && {
+  print_help
+  exit 0
+}
+#
+# Check if we've got a configuration file via the command-line switches. If so,
+# load it. Then, parse the arguments *again* because by convention they should
+# override the stuff given in the configuration file.
 [ -n "$CONF_FILE" ] && load_conffile "$CONF_FILE" && parse_args $@
+#
+# Print the config if so inclined.
+[ -n "$CONF_PRINT" ] && {
+  print_config
+  exit 0
+}
 #
 # We apparently have some configuration now, let's check its sanity.
 errors=0
@@ -227,27 +311,22 @@ errors=$(( $errors + $(check_CONF_DRY_RUN) ))
 errors=$(( $errors + $(check_CONF_INDEX_NAMES) ))
 errors=$(( $errors + $(check_CONF_SOURCE_DIR) ))
 errors=$(( $errors + $(check_CONF_DEST_DIR) ))
-[ "$errors" -gt 0 ] && die "$errors error(s) found, aborting."
+[ "$errors" -gt 0 ] && die "$errors error(s) found in the configuration, aborting."
 unset errors
 
-## Build the source path and check the sanity, too.
-#CONF_SOURCE_PATH="$CONF_SOURCE_DIR/$CONF_INDEX_NAMES"
-#errors=0
-#errors=$(( $errors + $(check_CONF_SOURCE_PATH) ))
-#[ "$errors" -gt 0 ] && die "$errors error(s) found, aborting."
-#unset errors
-
-# rsync source index to the destination
-run "rsync -a $CONF_SOURCE_PATH $CONF_DEST_DIR" || die "Oops, something went wrong."
-
-# Remove the source index
-run "rm -r $CONF_SOURCE_PATH" || die "Oops, something went wrong."
-
-# Create the symlink in place of the source index
-DST_IDX_PATH="$CONF_DEST_DIR/$CONF_INDEX_NAMES"
-run "ln -s $DST_IDX_PATH $CONF_SOURCE_DIR" || die "Oops, something went wrong."
-
-# exit with no errors
-exit 0
+# Do the actual work.
+errors=0
+for index_name in $CONF_INDEX_NAMES; do
+  symlink_index "$index_name" || {
+    log_error "Could not symlink index: '$index_name'."
+    errors=$(( $errors + 1 ))
+  }
+done
+if [ "$errors" -gt 0 ]; then
+  log_warning "Could not symlink $errors indices!"
+  return 2
+else
+  return 0
+fi
 
 # vim: set ts=2 sw=2 et cc=80:
